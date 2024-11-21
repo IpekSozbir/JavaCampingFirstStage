9ba8210787114726aeb122d2469d980cd35c004d
IpekSozbir <ipekoren@gmail.com>
2024-11-21 17:53:03 +0300

    first commit

---
+++ src/day01/C01_HelloWorld.java
@@ -0,0 +1,26 @@
+package day01;
+
+public class C01_HelloWorld {
+    public static void main(String[] args) {
+
+        // Java'da kullanicinin gorebilecegi mesajlari sout ile olustururuz
+
+        // Java buradaki yazilari yorum olarak kabul eder
+        // ve bu yazilari kod gibi calistirmaz
+
+        System.out.println("Hello World");
+
+        System.out.println("Java'nın ilk gününden merhaba...");
+
+        /*
+        Bu arada
+        yazilan
+        tüm satirlar
+        Java tarafindan
+        yorum olarak
+        kabul edilir
+         */
+
+
+    }
+}
